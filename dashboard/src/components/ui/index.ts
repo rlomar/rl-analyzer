343b06ffeabd6a7582d1193f847b81fc251a1b7d
rlomar <rlomar@github.com>
2026-05-30 00:47:45 -0700

    Remove dashboard, restore old frontend

--- dashboard/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-export { Button } from "./Button";
-export { Card } from "./Card";
-export { Badge } from "./Badge";
-export { Input } from "./Input";
-export { Select } from "./Select";
-export { Modal } from "./Modal";
